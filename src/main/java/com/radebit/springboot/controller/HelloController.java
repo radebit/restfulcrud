b4ae7ecee5bc426019ee1e3b6502cf743c365238
radebit <26564356@qq.com>
2019-03-21 15:43:46 +0800

    add

--- src/main/java/com/radebit/springboot/controller/HelloController.java
+++ src/main/java/com/radebit/springboot/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Controller
@@ -17,7 +18,8 @@ public class HelloController {
 
     @RequestMapping("/success")
     public String success(Map<String,Object> map){
-        map.put("hello","你好");
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
         //classpath:/templates/success.html
         return "success";
     }

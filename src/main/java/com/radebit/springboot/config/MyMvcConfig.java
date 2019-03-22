a841da134310dcd32c70f8520aceb53f5c29382a
radebit <26564356@qq.com>
2019-03-22 13:36:33 +0800

    add

--- src/main/java/com/radebit/springboot/config/MyMvcConfig.java
+++ src/main/java/com/radebit/springboot/config/MyMvcConfig.java
@@ -4,6 +4,7 @@ package com.radebit.springboot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -15,13 +16,27 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/radebit").setViewName("success");
     }
 
+    //已过时
     //所有的WebMvcConfigurerAdapter组件都会一起作用
-    @Bean   //将组件注册在容器中
-    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
-        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+//    @Bean   //将组件注册在容器中
+//    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+//        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addViewControllers(ViewControllerRegistry registry) {
+//                //super.addViewControllers(registry);
+//                registry.addViewController("/").setViewName("login");
+//                registry.addViewController("/index.html").setViewName("login");
+//            }
+//        };
+//        return adapter;
+//    }
+
+    //新写法
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        WebMvcConfigurer adapter = new WebMvcConfigurer() {
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
-                //super.addViewControllers(registry);
                 registry.addViewController("/").setViewName("login");
                 registry.addViewController("/index.html").setViewName("login");
             }

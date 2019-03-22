9ebb0beeeb6432a2d49cee4ee524b1905d2edca5
radebit <26564356@qq.com>
2019-03-22 13:18:32 +0800

    add

---
+++ src/main/java/com/radebit/springboot/config/MyMvcConfig.java
@@ -0,0 +1,31 @@
+package com.radebit.springboot.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //super.addViewControllers(registry);
+        //浏览器发送 /radebit 请求来到 success
+        registry.addViewController("/radebit").setViewName("success");
+    }
+
+    //所有的WebMvcConfigurerAdapter组件都会一起作用
+    @Bean   //将组件注册在容器中
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                //super.addViewControllers(registry);
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return adapter;
+    }
+}
--- src/main/java/com/radebit/springboot/controller/HelloController.java
+++ src/main/java/com/radebit/springboot/controller/HelloController.java
@@ -10,6 +10,12 @@ import java.util.Map;
 @Controller
 public class HelloController {
 
+
+//    @RequestMapping({"/","/index.html"})
+//    public String index(){
+//        return "index";
+//    }
+
     @ResponseBody
     @RequestMapping("/hello")
     public String hell(){
